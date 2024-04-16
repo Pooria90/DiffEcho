ef5a8ba3f1e6ac84bc0a84611bb189b272f4edc2
Pooria Ashrafian <61358399+Pooria90@users.noreply.github.com>
2024-04-15 17:16:20 -0700

    Updated the paths.

--- text_seg_conditioned/infer_controlnet.py
+++ text_seg_conditioned/infer_controlnet.py
@@ -5,6 +5,8 @@ from torchvision import transforms
 
 base_model_path = "path/to/sd" 		# this is the path to stable diffusion model. Mine was "./stable-diffusion-v1-5"
 controlnet_path = "path/to/cnet" 	# this the path to your controlnet checkpoint
+image_path = "path/to/input/image"	# this is the path to the input segmentation map
+save_path = "path/to/save/outputs"	# this is the path to save generated images
 
 controlnet = ControlNetModel.from_pretrained(controlnet_path, torch_dtype=torch.float16, use_safetensors=True, safety_checker=None)
 pipe = StableDiffusionControlNetPipeline.from_pretrained(
@@ -19,11 +21,11 @@ pipe.scheduler = UniPCMultistepScheduler.from_config(pipe.scheduler.config)
 
 pipe.enable_model_cpu_offload()
 
-control_image = load_image("path/to/segmentation-map")
+control_image = load_image(image_path)
 prompt = "an ultrasound image of heart in four-chamber view" # or replace "four" with "two"
 
 # generates 5 synthetic images from the control_image (segmentation map)
 generator = torch.manual_seed(0)
 for i in range(5):
 	image = pipe(prompt, num_inference_steps=50, image=control_image).images[0]
-	image.save(f"./infer-tmp/ch4_ed_frame_generated_22_{i}.png")
+	image.save(f"{save_path}/generated_{i}.png")
